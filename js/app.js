9c59bab0ce05b1d5b6601472b9ef8e1a82507961
Edyta Lysiak <edyta.lysiak@booking.com>
2017-03-03 18:39:32 +0100

    added a screen with list of tasks and who is responsible for it

--- js/app.js
+++ js/app.js
@@ -3,13 +3,12 @@ var app = angular.module("tasksList", []);
 (function (app) {
     "use strict";
     app.controller("peopleTasksList", function ($scope) {
-        $scope.people = [
-            {name: "John Snow"},
-            {name: "Tyrion Lannister"},
-            {name: "Daenerys Targaryen"},
-            {name: "Arya Stark"},
-            {name: "Cersei Lannister"},
-            {name: "Edyta Lysiak"}
+        $scope.peopleTasks = [
+            {name: "John Snow", description:"Protect the Wall from potential threats", done: false},
+            {name: "Tyrion Lannister", description:"Arrange meeting with representative of Astapor", done: false},
+            {name: "Daenerys Targaryen", description: "Prepare the conquest of Westeros strategy", done:false},
+            {name: "Cersei Lannister", description: "Open the Small Council meeting", done: false},
+            {name: "Edyta Lysiak", description: "Update the To Do List", done: false}
         ];
     });
 })(app);
