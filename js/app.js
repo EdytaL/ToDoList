0443ad7164afd60a4f5a7b91a002db638b1a34a6
Edyta Lysiak <edyta.lysiak@booking.com>
2017-03-03 20:31:52 +0100

    added input 'Add new task'

--- js/app.js
+++ js/app.js
@@ -4,12 +4,15 @@ var app = angular.module("tasksList", []);
     "use strict";
     app.controller("peopleTasksList", function ($scope) {
         $scope.peopleTasks = [
-            {name: "John Snow", description:"Protect the Wall from potential threats", done: false},
-            {name: "Tyrion Lannister", description:"Arrange meeting with representative of Astapor", done: false},
-            {name: "Daenerys Targaryen", description: "Prepare the conquest of Westeros strategy", done:false},
-            {name: "Cersei Lannister", description: "Open the Small Council meeting", done: false},
-            {name: "Edyta Lysiak", description: "Update the To Do List", done: false}
+            {name: "Jon Snow", description:"Protect the Wall from potential threats", done: false, image: "img/Jon_Snow.jpg"},
+            {name: "Tyrion Lannister", description:"Arrange meeting with representative of Astapor", done: false, image: "img/Tyrion_Lannister.jpg" },
+            {name: "Daenerys Targaryen", description: "Prepare the conquest of Westeros strategy", done:false, image: "img/Daenerys_Targaryen.jpg"},
+            {name: "Cersei Lannister", description: "Open the Small Council meeting", done: false, image:"img/Cersei_Lannister.jpg"},
+            {name: "Edyta Lysiak", description: "Update the To Do List", done: false, image:"img/Edyta_Lysiak.jpg"}
         ];
+        $scope.addTask = function() {
+            $scope.peopleTasks.push ({description: $scope.newTask, name: "Edyta Lysiak", done: false})
+        }
     });
 })(app);
 
