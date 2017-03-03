2e33930364846052b94078ef2e0dadfa88d1cc57
Edyta Lysiak <edyta.lysiak@booking.com>
2017-03-03 17:30:18 +0100

    added people list

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module("tasksList", []);
+
+(function (app) {
+    "use strict";
+    app.controller("peopleTasksList", function ($scope) {
+        $scope.people = [
+            {name: "John Snow"},
+            {name: "Tyrion Lannister"},
+            {name: "Daenerys Targaryen"},
+            {name: "Arya Stark"},
+            {name: "Cersei Lannister"},
+            {name: "Edyta Lysiak"}
+        ];
+    });
+})(app);
+
