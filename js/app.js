2409442d3251694a2f15c933ebac65047839e73a
Edyta Lysiak <edyta.lysiak@booking.com>
2017-03-04 17:54:28 +0100

    added button 'Clean Completed', functionality - mark completed task

--- js/app.js
+++ js/app.js
@@ -1,18 +1,38 @@
-var app = angular.module("tasksList", []);
+var app = angular.module("tasksList", ["xeditable"]);
 
 (function (app) {
     "use strict";
-    app.controller("peopleTasksList", function ($scope) {
-        $scope.peopleTasks = [
-            {name: "Jon Snow", description:"Protect the Wall from potential threats", done: false, image: "img/Jon_Snow.jpg"},
-            {name: "Tyrion Lannister", description:"Arrange meeting with representative of Astapor", done: false, image: "img/Tyrion_Lannister.jpg" },
-            {name: "Daenerys Targaryen", description: "Prepare the conquest of Westeros strategy", done:false, image: "img/Daenerys_Targaryen.jpg"},
-            {name: "Cersei Lannister", description: "Open the Small Council meeting", done: false, image:"img/Cersei_Lannister.jpg"},
-            {name: "Edyta Lysiak", description: "Update the To Do List", done: false, image:"img/Edyta_Lysiak.jpg"}
+    
+    app.run(function(editableOptions) {
+        editableOptions.theme = 'bs3'; 
+    });
+    
+    app.controller("peopleTasksList", function ($scope, $filter) {
+        $scope.peopleList = [
+            {name: "Jon Snow", image: "img/Jon_Snow.jpg"},
+            {name: "Tyrion Lannister", image: "img/Tyrion_Lannister.jpg" },
+            {name: "Daenerys Targaryen", image: "img/Daenerys_Targaryen.jpg"},
+            {name: "Cersei Lannister", image:"img/Cersei_Lannister.jpg"},
+            {name: "Edyta Lysiak", image:"img/Edyta_Lysiak.jpg"},
         ];
+        
+        $scope.peopleTasks = [
+            {name: "Jon Snow", description:"Protect the Wall from potential threats", done: false },
+            {name: "Tyrion Lannister", description:"Arrange meeting with representative of Astapor", done: false},
+            {name: "Daenerys Targaryen", description: "Prepare the conquest of Westeros strategy", done:false},
+            {name: "Cersei Lannister", description: "Open the Small Council meeting", done: false},
+            {name: "Edyta Lysiak", description: "Update the To Do List", done: false}
+        ];
+       
         $scope.addTask = function() {
-            $scope.peopleTasks.push ({description: $scope.newTask, name: "Edyta Lysiak", done: false})
-        }
+            $scope.peopleTasks.push ({description: $scope.newTask, name: "Edyta Lysiak", done: false});
+            $scope.newTask = "";
+        };
+        $scope.clearCompleted = function() {
+            $scope.peopleTasks =$scope.peopleTasks.filter(function(task) { 
+                return !task.done
+            });
+        };            
     });
 })(app);
 
