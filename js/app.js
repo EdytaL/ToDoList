220d1d75657a1b4f10517d6b56de38b713b2c0f1
Edyta Lysiak <edyta.lysiak@booking.com>
2017-03-05 18:13:46 +0100

    added select dropdown

--- js/app.js
+++ js/app.js
@@ -39,6 +39,24 @@ var app = angular.module("tasksList", ["xeditable"]);
         };     
        
     });
+    // filterBy implementation
+    app.filter('filterBy', function() {
+        return function(array, query) {
+
+            var parts = query && query.trim().split(/\s+/),
+                keys = Object.keys(array[0]);
+
+            if (!parts || !parts.length) return array;
+
+            return array.filter(function(obj) {
+                return parts.every(function(part) {
+                    return keys.some(function(key) {
+                        return String(obj[key]).toLowerCase().indexOf(part.toLowerCase()) > -1;
+                    });
+                });
+            });
+        };
+});
 })(app);
 
 
